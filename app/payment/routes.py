c45b366463ced0114e7185ae365a754b5e5bc373
Bha <bha@Jhsync254.afuya.co>
2025-05-25 11:24:15 +0300

    still progressing secure payment 6th attempt redirection implementation

--- app/payment/routes.py
+++ app/payment/routes.py
@@ -346,7 +346,8 @@ def thank_you(order_id):
             'payment_method': order.payment_method or "Credit Card",
             'order_total': float(order.total) if order.total else 0.00,
             #'customer_email': order.user.email if order.user else None,
-            'items': order.items() if hasattr(order, 'items') else []
+            'items': order.items if hasattr(order, 'items') else []
+
         }
 
         return render_template('payments/thank_you.html', **context)
